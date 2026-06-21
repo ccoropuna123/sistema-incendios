48e069cf5247fe99ea9733a29b87f0f196f7fc5e
ccoropuna123 <74697970@continental.edu.pe>
2026-06-20 19:36:56 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,8 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    // Intento de ponerlo en servidor para acceder con celular
-    
-    /*server: {
-        host: true,
-        port: 5173,
-    }, */
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+    }
 });
